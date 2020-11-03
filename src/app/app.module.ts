e698c183e7e7f97ea34b0e512dff5421196d28c8
Sulmaz <Sulmaz@DESKTOP-QTGHBCO>
2020-11-03 15:54:28 +0330

    add built-in validators

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainFormComponent } from './main-form/main-form.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { MainFormComponent } from './main-form/main-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/main-form/main-form.component.ts
+++ src/app/main-form/main-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-main-form',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-form.component.css']
 })
 export class MainFormComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  constructor() {
+    this.createForm();
+  }
 
   ngOnInit(): void {
   }
 
+  private createForm() {
+    this.form = new FormGroup({
+      personType: new FormControl(),
+      companyName : new FormControl('',
+        [Validators.required,
+          Validators.maxLength(10)]),
+      companyType: new FormControl(),
+      registrationNo: new FormControl('',
+        [Validators.pattern('^[0-9]*$'),
+        Validators.maxLength(10),
+          Validators.minLength(10)
+        ]),
+      registrationDate: new FormControl(),
+      email: new FormControl('',
+        [Validators.email,
+        Validators.maxLength(50)]
+        ),
+      postalCode: new FormControl('',
+        [Validators.maxLength(11),
+        Validators.pattern('^[0-9]*$')
+        ]) ,
+      phoneNo: new FormControl('',
+        Validators.pattern('^[0-9]*$')),
+      address: new FormControl('',
+        Validators.maxLength(400)
+        ),
+      firstMobileNo: new FormControl(),
+      secondMobileNo: new FormControl()
+    });
+  }
 }
